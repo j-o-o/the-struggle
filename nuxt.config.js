9d87dd2a55dd7281ea398a95aa9d762a636158ff
j-o-o <joooooo308@gmail.com>
2020-07-20 14:50:19 +0200

    asd

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     }
   },
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'The Struggle is Part of the Story',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
